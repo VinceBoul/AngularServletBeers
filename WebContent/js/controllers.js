6ceb9e1ead24c47588029c2bea2949c6e2c54338
VinceBoul <vtoullec@gmail.com>
2016-07-07 16:50:59 +0200

    L'édition d'une bière fonctionne :)

--- WebContent/js/controllers.js
+++ WebContent/js/controllers.js
@@ -3,9 +3,14 @@
 /* Controllers */
 
 angular.module('BeerControllers', [])
-	.controller('BeerListCtrl',	[ '$scope', 'Beer', function($scope, Beer) {
-		$scope.beers = Beer.query();
-		$scope.orderProp = 'alcohol';
+	.controller('BeerListCtrl',	[ '$scope', '$http', function($scope, $http) {
+		$http.get('BeerList').success(function(data) {
+			console.log(data);
+		      $scope.beers = data;
+		    });
+
+	    $scope.orderProp = 'alcohol';
+		
 	} ])
 	.controller( 'BeerDetailCtrl', [ '$scope', '$routeParams', 'Beer', function($scope, $routeParams, Beer) {
 		$scope.beer = Beer.get({
@@ -18,20 +23,47 @@ angular.module('BeerControllers', [])
 			$scope.mainImg = img;
 		}
 	} ])
-	.controller('BeerCreateCtrl', [ '$scope', '$http', function($scope, $http) {
+	.controller( 'BeerEditCtrl', [ '$scope', '$routeParams', '$http', 'Beer', function($scope, $routeParams, $http, Beer) {
+		$scope.beer = Beer.get({
+			beerId : $routeParams.beerId
+		}, function(beer) {
+			$scope.beer = beer;
+		});
+
+		$scope.update = function(beerId) {
+			$http({
+				method : 'POST',
+				url : 'BeerEdit',
+				data : beerId, // forms user object
+				headers : {
+					'Content-Type' : 'application/x-www-form-urlencoded'
+				}
+			}).success(function(data) {
+				if (data.errors) {
+					// Showing errors.
+					$scope.errorName = data.errors.name;
+					$scope.errorUserName = data.errors.username;
+					$scope.errorEmail = data.errors.email;
+				} else {
+					console.log(data);
+					//$window.location.href = '#/beers';
+				}
+			});
+		}
+	} ])
+	.controller('BeerCreateCtrl', [ '$scope', '$http', '$window', function($scope, $http, $window) {
 	$scope.master = {};
 	// create a blank object to handle form data.
 	$scope.beer = {};
 	// calling our submit function.
 
 	$scope.update = function(beer) {
-		console.log($scope.beer);
 		console.log(beer);
 		// Posting data to php file
 		$http({
 			method : 'POST',
 			url : 'BeerCreate',
-			data : $scope.beer, // forms user object
+			data : beer, // forms user object
 			headers : {
 				'Content-Type' : 'application/x-www-form-urlencoded'
 			}
@@ -43,8 +75,7 @@ angular.module('BeerControllers', [])
 				$scope.errorEmail = data.errors.email;
 			} else {
 				console.log(data);
-
-				location.$scope.message = data.message;
+				$window.location.href = '#/beers';
 			}
 		});
 	};
